bed1997a1f3b3bc94e7a466829114663cc9566fb
Kelvin Tsoi <yltsoi@gmail.com>
2026-08-02 12:23:57 +0100

    Init - First Call with typescript

---
+++ firstCall.ts
@@ -0,0 +1,21 @@
+import Anthropic from "@anthropic-ai/sdk";
+import "dotenv/config";
+
+const client = new Anthropic();
+
+const messages = await client.messages.create({
+    model: "claude-haiku-4-5",
+    max_tokens: 256,
+    messages: [{        
+        role: "user",
+        content: "What is the Claude API ?"
+    }],
+});
+
+console.log(messages.content[0].text);
+
+### To RUn
+#   npx tsx firstCall.ts
+###
+
+
